4d8f2aa1ae101947f8963262e7e64b2e462f356c
dbronner18 <bronnerdevon@gmail.com>
2022-04-15 22:52:53 -0400

    Movie List App Start

---
+++ app/src/main/java/com/hfad/movielistapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hfad.movielistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    /* Supposed to allow share button to show on tool bar but I must have missed something since this crashes program
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the app bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        //need to add favorites list here as parameter
+        setShareActionIntent("*Favorites Movie List Here*");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setShareActionIntent (String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+    */
+
+}
